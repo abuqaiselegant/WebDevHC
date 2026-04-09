1c55c247bcd7f19043ff1883cb909fedc3cfcef2
Abu Qais <abuqaiselegant@gmail.com>
2026-04-09 23:37:45 +0530

    added new work

---
+++ authNotesApp/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const notes = [];
+
+
+
+app.post("/signup", (req,res)=>{
+    
+})
+
+app.post("/signin", (req,res)=>{
+
+})
+
+app.post("/notes",(req,res)=>{
+
+})
+
+app.get("/notes", (req, res)=>{
+
+})
+
+
+app.get("/signup",(req,res)=>{
+    
+})
+
+app.get("/signin", (req, res) =>{ 
+
+})
+
+app.get("/", (req,res)=>{
+
+})
+
+
+
+app.listen(3000, ()=>{
+
+})
+
