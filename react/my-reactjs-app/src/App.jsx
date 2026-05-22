46f3c596f5c6d9f6cb7db5067c839c327e0943ae
Abu Qais <abuqaiselegant@gmail.com>
2026-05-22 21:33:55 +0530

    revised components and props

--- react/my-reactjs-app/src/App.jsx
+++ react/my-reactjs-app/src/App.jsx
@@ -2,6 +2,7 @@ import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import Food from './Food.jsx'
 import Card from './Card.jsx'
+import Student from './Student.jsx'
 
 function App() {
 
@@ -9,9 +10,11 @@ function App() {
     <>
       <Header/>
       <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
+      <Student name ="Umar" age ={26} isStudent = {true}></Student>
+      <Student name ="Abu" age ={26} isStudent = {true}></Student>
+      <Student name ="anabiya" age ={6} isStudent = {false}></Student>
+      <Student name ="rimpo" age ={16} isStudent = {true}></Student>
+
     </>
   );
 }
