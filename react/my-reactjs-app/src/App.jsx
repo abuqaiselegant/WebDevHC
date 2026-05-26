8947c6a92ddd4538ca4e3cb68a77536d133edd95
Abu Qais <abuqaiselegant@gmail.com>
2026-05-26 21:30:05 +0530

    added new component

--- react/my-reactjs-app/src/App.jsx
+++ react/my-reactjs-app/src/App.jsx
@@ -19,12 +19,19 @@ function App() {
                   {id:5, name:"carrots",calories:35},
                   {id:6, name:"pinee",calories:91}];
 
+  const fastFood = [{id:1, name:"lays",calories:953},
+                  {id:2, name:"kurkure",calories:95},
+                  {id:3, name:"celery",calories:511},
+                  {id:4, name:"peanut",calories:315},
+                  {id:5, name:"carrots",calories:35},
+                  {id:6, name:"plum",calories:91}];
+
   return (<>
         {/* {fruits.length > 0 ? <List items ={fruits} category ="Fruits"/> : null}
         {Vegetables.length > 0 ? <List items ={Vegetables} category ="Vegetables"/>:null} */
         fruits.length >0 && <List items ={fruits} category ="fruits"/>}
-       {Vegetables.length >0 && <List items ={Vegetables} category ="Vegetables"/>
-}
+       {Vegetables.length >0 && <List items ={Vegetables} category ="Vegetables"/>}
+       {fastFood.length >0 && <List items ={fastFood} category ="fastFood"/>}
         </>
   );
 }
