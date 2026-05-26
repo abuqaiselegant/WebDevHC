9236968a8bed1b6fd3445f485605bf135a9c8ae0
Abu Qais <abuqaiselegant@gmail.com>
2026-05-26 21:30:43 +0530

    updated comments

--- react/my-reactjs-app/src/App.jsx
+++ react/my-reactjs-app/src/App.jsx
@@ -1,8 +1,8 @@
-import Header from './Header.jsx'
-import Footer from './Footer.jsx'
-import Food from './Food.jsx'
-import Card from './Card.jsx'
-import Student from './Student.jsx'
+// import Header from './Header.jsx'
+// import Footer from './Footer.jsx'
+// import Food from './Food.jsx'
+// import Card from './Card.jsx'
+// import Student from './Student.jsx'
 import List from './List.jsx'
 
 function App() {
