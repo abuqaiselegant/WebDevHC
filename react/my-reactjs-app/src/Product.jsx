154fbfa6abe5d80da04cebb131b24fec4824e958
Abu Qais <abuqaiselegant@gmail.com>
2026-05-30 23:13:14 +0530

    practice more useState

--- react/my-reactjs-app/src/Product.jsx
+++ react/my-reactjs-app/src/Product.jsx
@@ -1,24 +1,17 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-type Product = {
-  id: number
-  name: string
-  price: number
-  discount?: number  // optional
-}
-
-const allProducts: Product[] = [
+const allProducts = [
   { id: 1, name: "Headphones", price: 49.99, discount: 10 },
   { id: 2, name: "Keyboard", price: 29.99 },
   { id: 3, name: "Mouse", price: 19.99, discount: 5 },
-]
+];
 
 function ProductList() {
-  const [products, setProducts] = useState<Product[]>(allProducts)
+  const [products, setProducts] = useState(allProducts);
 
   return (
     <div>
-      {products.map(product => (
+      {products.map((product) => (
         <div key={product.id}>
           <h3>{product.name}</h3>
           <p>£{product.price}</p>
@@ -26,5 +19,7 @@ function ProductList() {
         </div>
       ))}
     </div>
-  )
+  );
 }
+
+export default ProductList;
